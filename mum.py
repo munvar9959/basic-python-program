6f3039e8d3b4129aea4446b007ee865a450ad5ed
munvar9959 <syed.munvar9959@gmail.com>
2025-09-17 16:12:33 +0530

    Add files via upload

---
+++ mum.py
@@ -0,0 +1,44 @@
+
+day=int(input("enter the name:"))
+if(day==1):
+    print("monday")
+elif(day==2):
+    print("tuesday")
+elif(day==3):
+    print("wednesday")
+elif(day==4):
+    print("thursday")
+elif(day==5):
+    print("friday")
+elif(day==6):
+    print("sartday")
+elif(day==7):
+    print("sunday")
+else:
+    print("error")
+# write a python program to check the day
+
+n=int(input("entern the age:"))
+if n>12:
+    print("child")
+elif n>12 and n<12:
+    print("teen")
+else:
+    print("adult")
+
+#2write a python program to check the fever
+temp:int(input("body temp:"))
+if(temp>=100):
+    print("fever")
+elif(temp==96):
+    print("normal")
+elif(temp<=80):
+    print("cold")
+else:
+    print("invalid")
+
+
+
+
+
+
